6664a81f89fb03d48e2e3244abdcb008ac6ad59c
S530491 <S530491@NWMISSOURI.EDU>
2018-06-29 23:27:21 -0500

    Add login, sign up web pages

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var http = require('http')
+var path = require('path')
+let https = require('https');
+
+const app = express()
+
+
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+app.set('views', path.join(__dirname, '/views'))
+
+
+const routes = require('./controllers')
+app.use('/', routes) 
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+    console.log( 'Node server is running on port ' + app.get( 'port' ));
+    });
