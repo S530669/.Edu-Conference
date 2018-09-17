3826e01c5e3655e095ed46c1723b6f3350c16147
DarshanReddy <S530669@nwmissouri.edu>
2018-09-17 09:47:47 -0500

    Fix the models(attendee & presenter) issue

---
+++ models/attendee.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Attendee = new Schema({
+    "name": {type:String, required:true},
+    "email": {type: String, required: true},
+    "contact": {type: String, required: true},
+    "school": {type: String, required: false},
+	"program": {type: String, required: true},
+    "presenter": {type:String, required:true}, 
+    "food": {type:String, required:true},
+    "restrict": {type:String, required:true}
+    });
+
+module.exports =  mongoose.model('Attendee', Attendee);
+
+
+
+
