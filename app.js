cf7e95315bf9edcc403900fa9b7c9c34bfc2c6a7
DarshanReddy <S530669@nwmissouri.edu>
2018-10-01 22:51:10 -0500

    Integrate admin functionalities with existing application

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ var couponCode = require('coupon-code');
 var Promise = require('bluebird');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
+var cookieParser = require('cookie-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+
+
+
 var routes = require('./routes/index');
+var users = require('./routes/users');
 
 mongoose.connect('mongodb://localhost:27017/conference');
 var Attendee = require('./models/attendee.js');
@@ -19,15 +31,21 @@ var vendor = require('./models/vendor.js');
 
 
 
-app.set("views", path.resolve(__dirname, "views")) 
-app.set('view engine', 'html') 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 var code = couponCode.generate();
 var count = 0;
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json());
+
 app.use('/', routes);
+app.use('/users', users);
 app.use(express.static(path.join(__dirname, 'Assets')));
 app.use(express.static('./'));
 
@@ -228,6 +246,56 @@ transporter.sendMail(mailOptions, function(error, info){
   }
 });
 });
+
+//----Admin----
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+
+
 
 app.set('port',(process.env.PORT || 8082));
 
--- routes/index.js
+++ routes/index.js
@@ -52,4 +52,18 @@ router.get('/', function(request, response) {
     response.render('edupay.ejs');
   });
 
+//   // Get Homepage
+// router.get('/', ensureAuthenticated, function(req, res){
+// 	res.render('index');
+// });
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/users/login');
+	}
+}
+
   module.exports = router;
