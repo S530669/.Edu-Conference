47af38a3acb7fc5d3a16be24150d15be48c5379e
DarshanReddy <S530669@nwmissouri.edu>
2018-10-03 01:16:48 -0500

    Add code to fetch total number of attendees

--- routes/index.js
+++ routes/index.js
@@ -55,9 +55,12 @@ router.get('/', function(request, response) {
 router.get('/admin', ensureAuthenticated,  function(req, res){ 
 	res.render('/');
 });
+
 router.get("/adminhomepage", function(request, response) {
 	response.render('adminhomepage.ejs');
   });
+
+
 
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
