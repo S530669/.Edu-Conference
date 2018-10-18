333ef6f3380ee45c558c3b35f777496af3240280
S530491 <S530491@NWMISSOURI.EDU>
2018-10-18 13:36:21 -0500

    Fix flash message errors

--- routes/index.js
+++ routes/index.js
@@ -1,140 +1,140 @@
 var express = require('express');
 var router = express.Router();
-var path = require ('path');
+var path = require('path');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-router.get('/', function(request, response) {
-    response.render('homepage.ejs');
-  });
-  
-  router.get('/homepage', function(request, response) {
-    response.render('homepage.ejs');
-  }); 
-  
-  router.get('/attendee', function(request, response) {
-    response.render('attendee.ejs');
-  });
-  
-  router.get("/presenter", function(request, response) {
-    response.render('presenter.ejs');
-  });
+router.get('/', function (request, response) {
+  response.render('homepage.ejs');
+});
 
-  router.get("/Graduatestudent", function(request, response) {
-    response.render('Graduatestudent.ejs');
-  });
-  
-  router.get("/faculty", function(request, response) {
-    response.render('faculty.ejs');
-  });
-  
-  router.get("/vendor", function(request, response) {
-    response.render('vendor.ejs');
-  });
-  
-  router.get("/cart", function(request, response) {
-      response.render('cart.ejs'); 
-  });
+router.get('/homepage', function (request, response) {
+  response.render('homepage.ejs');
+});
 
-  router.get("/travel", function(request, response) {
-    response.render('travel.ejs');
-  });
+router.get('/attendee', function (request, response) {
+  response.render('attendee.ejs');
+});
 
-  router.get("/schedule", function(request, response) {
-    response.render('schedule.ejs');
-  });
+router.get("/presenter", function (request, response) {
+  response.render('presenter.ejs');
+});
 
-  router.get("/program", function(request, response) {
-    response.render('program.ejs');
-  });
-  
-  router.get("/deadlines", function(request, response) {
-    response.render('deadlines.ejs');
-  });
-  router.get("/contact", function(request, response) {
-    response.render('contact.ejs');
-  });
+router.get("/Graduatestudent", function (request, response) {
+  response.render('Graduatestudent.ejs');
+});
 
-  router.get("/couponcode", function(request, response) {
-    response.render('couponcode.ejs');
-  });
-  
-  router.get("/Paymentthroughcheck", function(request, response){
-    response.render('Paymentthroughcheck.ejs');
-  });
-  router.get("/PayThroughCards", function(request, response){
-    response.render('PayThroughCards.ejs');
-  });
-  router.get("/edupay", function(request, response){
-    response.render('edupay.ejs');
-  });
-  router.get("/presenterconf", function(request, response){
-    response.render('presenterconf');
-  });
+router.get("/faculty", function (request, response) {
+  response.render('faculty.ejs');
+});
 
-  router.get('/forgotE',  function(req, res){ 
-    res.render('forgotE.ejs');
-  });
-  
+router.get("/vendor", function (request, response) {
+  response.render('vendor.ejs');
+});
 
-router.get('/admin', ensureAuthenticated,  function(req, res){ 
-	res.render('/');
+router.get("/cart", function (request, response) {
+  response.render('cart.ejs');
+});
+
+router.get("/travel", function (request, response) {
+  response.render('travel.ejs');
+});
+
+router.get("/schedule", function (request, response) {
+  response.render('schedule.ejs');
+});
+
+router.get("/program", function (request, response) {
+  response.render('program.ejs');
+});
+
+router.get("/deadlines", function (request, response) {
+  response.render('deadlines.ejs');
+});
+router.get("/contact", function (request, response) {
+  response.render('contact.ejs');
+});
+
+router.get("/couponcode", function (request, response) {
+  response.render('couponcode.ejs');
+});
+
+router.get("/Paymentthroughcheck", function (request, response) {
+  response.render('Paymentthroughcheck.ejs');
+});
+router.get("/PayThroughCards", function (request, response) {
+  response.render('PayThroughCards.ejs');
+});
+router.get("/edupay", function (request, response) {
+  response.render('edupay.ejs');
+});
+router.get("/presenterconf", function (request, response) {
+  response.render('presenterconf');
+});
+
+router.get('/forgotE', function (req, res) {
+  res.render('forgotE.ejs');
+});
+
+
+router.get('/admin', ensureAuthenticated, function (req, res) {
+  res.render('adminhomepage.ejs');
 });
 
 
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    req.flash('error_msg', 'You are not logged in');
+    res.redirect('/users/login');
   }
 
-  router.get("/adminhomepage", function(request, response) {
+  router.get("/adminhomepage", function (request, response) {
     response.render('adminhomepage.ejs');
+  });
+  router.get("/Deadlines", function (request, response) {
+    response.render('UpdateDeadlines.ejs');
+  });
+  router.get("/FeeDetails", function (request, response) {
+    response.render('UpdateFeeDetails.ejs');
+  });
+  router.get("/ProgramDetails", function (request, response) {
+    response.render('UpdateProgramDetails.ejs');
+  });
+
+
+  router.get('/adminattendee', function (request, response) {
+    db.collection('attendees').find().toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      response.render('adminattendee.ejs', { list: result });
+    })
+  });
+
+  router.get('/adminvendor', (request, response, next) => {
+    db.collection('vendors').find().toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      response.render('adminvendor.ejs', { list: result });
+    })
+  })
+  router.get('/admincontact', (request, response, next) => {
+    db.collection('contacts').find().toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      response.render('admincontact.ejs', { list: result });
+    })
+  })
+
+  router.get("/AdminPresenter", function (request, response) {
+    db.collection('presenters').find().toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      response.render('AdminPresenter.ejs', { list: result });
+
     });
-    router.get("/Deadlines", function(request, response) {
-      response.render('UpdateDeadlines.ejs');
-      });
-      router.get("/FeeDetails", function(request, response) {
-        response.render('UpdateFeeDetails.ejs');
-        });
-        router.get("/ProgramDetails", function(request, response) {
-          response.render('UpdateProgramDetails.ejs');
-          });
-
-          
-    router.get('/adminattendee', function(request, response){ 
-      db.collection('attendees').find().toArray(function(err,result){
-        if (err) throw err;
-        console.log(result);
-        response.render('adminattendee.ejs',{list : result});
-      })
-      });
-
-     router.get('/adminvendor', (request, response, next) => {
-    db.collection('vendors').find().toArray(function(err,result){
-        if (err) throw err;
-        console.log(result);
-        response.render('adminvendor.ejs',{list : result});
-      })
-      })
-      router.get('/admincontact', (request, response, next) => {
-        db.collection('contacts').find().toArray(function(err,result){
-            if (err) throw err;
-            console.log(result);
-            response.render('admincontact.ejs',{list : result});
-          })
-          })
-
-router.get("/AdminPresenter", function(request, response) {
-  db.collection('presenters').find().toArray(function(err,result){
-    if (err) throw err;
-    console.log(result);
-    response.render('AdminPresenter.ejs',{list : result});
-  
-});
-});
+  });
 }
 
 
@@ -149,4 +149,4 @@ router.get("/AdminPresenter", function(request, response) {
 //   });
 // });
 
-  module.exports = router;
+module.exports = router;
