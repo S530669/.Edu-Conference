23059bd2e8188fafb4a070f073df023971edd8e7
S530714 <S530714@nwmissouri.edu>
2018-11-11 12:30:14 -0600

    Add testing for program details

--- selenium/faculty.js
+++ selenium/faculty.js
@@ -8,25 +8,10 @@ var driver = new webdriver.Builder()
 .build()
 
 driver.get('localhost:8082/faculty');
-driver.findElement(By.name('email1')).sendKeys('anusha@gmail.com');
-driver.findElement(By.name('generate')).click();
 driver.findElement(By.name('fname')).sendKeys('Anusha');
 driver.findElement(By.name('lname')).sendKeys('Kollu');
-driver.findElement(By.name('email')).sendKeys('anusha@gmail.com');
+driver.findElement(By.name('email')).sendKeys('anusha12@gmail.com');
 driver.findElement(By.name('contact')).sendKeys('6605280506');
 driver.findElement(By.name('program')).sendKeys('It');
 driver.findElement(By.name('submit')).click();
 
-function check_title() {
-    var promise = driver.getTitle().then( function(title){
-        if(title === 'wiki - Google Search')
-        {
-            console.log('success');
-            return true
-        }
-        else{
-            console.log('fail --'+ title);
-        }
-    });
-    return promise;
-}
