c9a4bcecc6b8cdad08a8130b88a46bf5dea45143
s530664 <s530664@nwmissouri.edu>
2018-11-27 23:10:58 -0600

    Add file upload option and fix font issues

--- models/presenter.js
+++ models/presenter.js
@@ -9,6 +9,7 @@ var Presenter = new Schema({
     "target": { type: String, required: true },
     "topic": { type: String, required: true },
     "attach": { type: String, required: false },
+    "attach1": { type: String, required: false },
     "subject": { type: String, required: true },
     "food": { type: String, required: false },
     "confirm": { type: String, required: false },
